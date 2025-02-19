75784db6ea2338831bca54d03e2582e133eab064
John Merryweather Cooper <jmcooper8654@gmail.com>
2025-02-19 07:40:22 -0600

    Implement some new constructors

--- PowerShellModules/Fix-Octopus.Client-Scripts/Common/OctopusClientModule.psm1
+++ PowerShellModules/Fix-Octopus.Client-Scripts/Common/OctopusClientModule.psm1
@@ -24,6 +24,85 @@ function Get-AccountByName {
 
         Add-Type -Path 'Octopus.Client.dll'
     }
+
+    PROCESS {
+        $Repository.Accounts.FindByName($AccountName) | Write-Output
+    }
+}
+
+<#
+    Get-Action
+#>
+function Get-Action {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        $Step
+    )
+
+    BEGIN {
+        Set-StrictMode -Version 3.0
+        Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+        Add-Type -Path 'Octopus.Client.dll'
+    }
+
+    PROCESS {
+        $Step.actions | Write-Output
+    }
+}
+
+<#
+    Get-ActionProperty
+#>
+function Get-ActionProperty {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        $Step,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Octopus.Action.Azure.AccountId')]
+        [string]
+        $Property
+    )
+
+    BEGIN {
+        Set-StrictMode -Version 3.0
+        Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+        Add-Type -Path 'Octopus.Client.dll'
+    }
+
+    PROCESS {
+        $Step | Get-Action | ForEach-Object -Process { $_.Properties[$Property].value | Write-Output }
+    }
+}
+
+<#
+    Get-DeploymentProcess
+#>
+function Get-DeploymentProcess {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Octopus.Client.OctopusRepository]
+        $Repository,
+
+        [Parameter(Mandatory)]
+        $Project
+    )
+
+    BEGIN {
+        Set-StrictMode -Version 3.0
+        Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+        Add-Type -Path 'Octopus.Client.dll'
+    }
+
+    PROCESS {
+        $Repository.DeploymentProcesses.Get($Project.deploymentprocessid)
+    }
 }
 
 <#
@@ -31,14 +110,11 @@ function Get-AccountByName {
 #>
 function Get-Project {
     [CmdletBinding()]
+    [OutputType([Octopus.Client.])]
     param (
         [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [Octopus.Client.OctopusRepository]
         $Repository,
-
-        [ValidateNotNullOrEmpty()]
-        [switchblock]
-        $FilterScript,
 
         [ValidateNotNullOrEmpty()]
         [string]
@@ -59,9 +135,6 @@ function Get-Project {
         if ($All.IsPresent) {
             $Repository.Projects.FindAll() | Write-Output
         }
-        elseif ($PSBoundParameters.ContainsKey('FilterScript')) {
-            $Repository.Projects.FindAll() | Where-Object -FilterScript $FilterScript | Write-Output
-        }
         elseif ($PSBoundParameters.ContainsKey('Name')) {
             $Repository.Projects.FindAll() | Where-Object -Property Name -EQ $Name | Write-Output
         }
@@ -69,6 +142,136 @@ function Get-Project {
             return $null
         }
     }
+}
+
+<#
+    Get-Step
+#>
+function Get-Step {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        $DeploymentProcess
+    )
+
+    BEGIN {
+        Set-StrictMode -Version 3.0
+        Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+        Add-Type -Path 'Octopus.Client.dll'
+    }
+
+    PROCESS {
+        $DeploymentProcess.steps | Write-Output
+    }
+}
+
+<#
+    Initialize-AzureServicePrincipal
+#>
+function Initialize-AzureServicePrincipal {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Octopus.Client.Model.Accounts.AzureServicePrincipalAccountResource]
+        $AzureServicePrincipal,
+
+        [hashtable]
+        $PropertyValue
+    )
+
+    BEGIN {
+        Set-StrictMode -Version 3.0
+        Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+        Add-Type -Path 'Octopus.Client.dll'
+    }
+
+    PROCESS {
+        $AzureServicePrincipal | ForEach-Object -Process {
+            if (-not $PSBoundParameters.ContainsKey('PropertyValue') -or ($null -eq $PropertyValue) -or ($PropertyValue.Keys.Count -lt 1)) {
+                return $_
+            }
+
+            if ($PropertyValue.ContainsKey('ClientId')) {
+                $_.ClientId = $PropertyValue['ClientId']
+            }
+
+            if ($PropertyValue.ContainsKey('TenantId')) {
+                $_.TenantId = $PropertyValue['TenantId']
+            }
+
+            if ($PropertyValue.ContainsKey('Description')) {
+                $_.Description = $PropertyValue['Description']
+            }
+
+            if ($PropertyValue.ContainsKey('Name')) {
+                $_.Name = $PropertyValue['Name']
+            }
+
+            if ($PropertyValue.ContainsKey('Password')) {
+                $_.Password = $PropertyValue['Password']
+            }
+
+            if ($PropertyValue.ContainsKey('SubscriptionNumber')) {
+                $_.SubscriptionNumber = $PropertyValue['SubscriptionNumber']
+            }
+
+            if ($PropertyValue.ContainsKey('TenantedDeploymentParticipation')) {
+                $_.TenantedDeploymentParticipation = $PropertyValue['TenantedDeploymentParticipation']
+            }
+
+            if ($PropertyValue.ContainsKey('TenantTags')) {
+                $_.TenantTags = $PropertyValue['TenantTags']
+            }
+
+            if ($PropertyValue.ContainsKey('TenantIds')) {
+                $_.TenantIds = $PropertyValue['TenantIds']
+            }
+
+            if ($PropertyValue.ContainsKey('EnvironmentIds')) {
+                $_.EnvironmentIds = $PropertyValue['EnvironmentIds']
+            }
+
+            $_ | Write-Output
+        }
+    }
+}
+
+<#
+    New-AzureServicePrincipal
+#>
+function New-AzureServicePrincipal {
+    [CmdletBinding()]
+    [OutputType([Octopus.Client.Model.Accounts.AzureServicePrincipalAccountResource])]
+    param ()
+
+    Set-StrictMode -Version 3.0
+    Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+    Add-Type -Path 'Octopus.Client.dll'
+
+    New-Object -TypeName Octopus.Client.Model.Accounts.AzureServicePrincipalAccountResource | Write-Output
+}
+
+<#
+    New-Client
+#>
+function New-Client {
+    [CmdletBinding()]
+    [OutputType([Octopus.Client.OctopusClient])]
+    param (
+        [Parameter(Mandatory)]
+        [Octopus.Client.OctopusServer.Endpoint]
+        $Endpoint
+    )
+
+    Set-StrictMode -Version 3.0
+    Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+    Add-Type -Path 'Octopus.Client.dll'
+
+    New-Object -TypeName Octopus.Client.OctopusClient -ArgumentList $Endpoint
 }
 
 <#
@@ -100,21 +303,54 @@ function New-Endpoint {
     New-Repository
 #>
 function New-Repository {
-    [CmdletBinding(SupportsShouldProcess)]
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'UsingEndpoint')]
+    [OutputType(Octopus.Client.OctopusRepository)]
     param (
-        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Parameter(Mandatory, ParameterSetName = 'UsingEndpoint')]
         [Octopus.Client.OctopusServerEndpoint]
-        $Endpoint
+        $Endpoint,
+
+        [Parameter(Mandatory, ParameterSetName = 'UsingClient')]
+        [Octopus.Client.OctopusClient]
+        $Client,
+
+        [Parameter(Mandatory, ParameterSetName = 'UsingClient')]
+        $Space
     )
 
-    BEGIN {
-        Set-StrictMode -Version 3.0
-        Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+    Set-StrictMode -Version 3.0
+    Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
 
-        Add-Type -Path 'Octopus.Client.dll'
+    Add-Type -Path 'Octopus.Client.dll'
+
+    if ($PSCmdlet.ParameterSetName -eq 'UsingClient') {
+        $Client.ForSpace($Space) | Write-Output
     }
-
-    PROCESS {
+    else {
         New-Object -TypeName Octopus.Client.OctopusRepository -ArgumentList $Endpoint | Write-Output
     }
+}
+
+<#
+    New-Space
+#>
+function New-Space {
+    [CmdletBinding(SupportsShouldProcess)]
+    [OutputType([])]
+    param (
+        [Paramter(Mandatory)]
+        [Octopus.Client.OctopusRepository]
+        $Repository,
+
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $SpaceName = 'default'
+    )
+
+    Set-StrictMode -Version 3.0
+    Set-Variable -Name ScriptName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+    Add-Type -Path 'Octopus.Client.dll'
+
+    $Repository.Spaces.FindByName($SpaceName) | Write-Output
 }
